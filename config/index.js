9c8d994ff4d8279de76f431831d65e309cd5dbfe
clayzhang <clayzhang@tencent.com>
2018-12-29 01:25:20 +0800

    [ITERATION] update wepack config

--- config/index.js
+++ config/index.js
@@ -43,7 +43,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/cinext/',
+    // assetsPublicPath: '/cinext/',
+    assetsPublicPath: 'http://g1.icinext.com/cinext/',
 
     /**
      * Source Maps
